5238c5a0ac257a164c7dc0ffca7efb52644d1f8f
Cedric Zitzmann <cedric.zitzmann@gmail.com>
2021-05-16 19:48:04 +0200

    fixed type

--- src/permissions/userDetailsService.ts
+++ src/permissions/userDetailsService.ts
@@ -1,7 +1,7 @@
 import { UserDetails } from "./userDetails";
-import Express from "express";
+import { Request } from "express";
 
 export abstract class UserDetailsService {
     public abstract loadUserDetails(userId: string): UserDetails;
-    public abstract resolveUserIdFromRequest(request: Express.Request): string;
+    public abstract resolveUserIdFromRequest(request: Request): string;
 }
