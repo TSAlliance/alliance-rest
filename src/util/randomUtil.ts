6724c2ebf986e83a1e6b0d90648784b3b4613995
Cedric Zitzmann <cedric.zitzmann@gmail.com>
2021-08-22 16:43:30 +0200

    added random string generation

--- src/util/randomUtil.ts
+++ src/util/randomUtil.ts
@@ -8,4 +8,23 @@ export class RandomUtil {
     public static randomCredentialHash(): string {
         return randomBytes(8).toString("hex");
     }
+
+    public static generateClientId(): string {
+        return this.randomString(16);
+    }
+
+    public static generateClientSecret(): string {
+        return this.randomString(32);
+    }
+
+    public static randomString(length = 8): string {
+        const chars = "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789";
+
+        let str = "";
+        for (let i = 0; i < length; i++) {
+            str += chars.charAt(Math.floor(Math.random() * chars.length));
+        }
+
+        return str;
+    }
 }
