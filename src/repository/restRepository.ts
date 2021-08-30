897cbd5b9fa9614b007daf2067ea58e0de14878c
Zitzmann <cedric.zitzmann@gmail.com>
2021-08-30 15:44:04 +0200

    added findById

--- src/repository/restRepository.ts
+++ src/repository/restRepository.ts
@@ -1,8 +1,26 @@
 import { PageableRepository } from "nestjs-pager";
-import { FindManyOptions } from "typeorm";
+import { FindManyOptions, ObjectID } from "typeorm";
+
+export class FindId { 
+    colName: string;
+    value: string | number | Date | ObjectID 
+}
 
 export class RestRepository<T> extends PageableRepository<T> {
+
+    public async findById(id: string | number | Date | ObjectID | FindId, options?: FindManyOptions<T>): Promise<T> {
+        if(id instanceof FindId) {
+            const whereClause = {...options}
+            whereClause[id.colName] = id.value;
+
+            return this.findOne({ where: whereClause })
+        } else {
+            return this.findOne(id, options);
+        }
+    }
+
     public async exists(options?: FindManyOptions<T>): Promise<boolean> {
+        this.findOne
         return !!(await this.findOne(options));
     }
 }
