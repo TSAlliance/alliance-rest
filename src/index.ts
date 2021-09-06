74d9a4a77927e787ee358be6c2e7bf80c000d462
Cedric Zitzmann <cedric.zitzmann@gmail.com>
2021-09-06 17:35:45 +0200

    removed

--- src/index.ts
+++ src/index.ts
@@ -13,14 +13,3 @@ export { ValidatorModule, Validator, ValidationRule };
 
 import { RestRepository } from "./repository/restRepository";
 export { RestRepository };
-
-const validator = new Validator();
-console.log("Valid? >> ", validator.text("test", null).check()); // False, but does not throw error
-console.log("Valid? >> ", validator.text("test", "hello").check()); // True
-console.log("Valid? >> ", validator.text("test", undefined).required().check()); // False and collects an error to throw below
-
-try {
-    validator.throwErrors();
-} catch (err) {
-    console.log(err);
-}
